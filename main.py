2d5b5b1096ad16f0098bd1898c6cef9c9ac249d9
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-26 19:51:18 +0300

    get_add_image endpoint added to make post request to api

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import jsonify, request
-from helper import get_application_name, get_application_id
-from model import fetch_data, close_connection
+from helper import get_application_name, get_application_id, get_add_image_params
+from model import fetch_data, close_connection, create_record
+from datetime import datetime
 
 
 # TODO method documentation
@@ -77,6 +78,67 @@ def retrieve_app_id():
     # authentication or query based reasons
     except Exception as error:
         return f'{error}'
+
+
+# http://localhost:80//apps-api/v1/images/post?app-id=41?image-path=asda
+@app.route('/apps-api/v1/images/post', methods=['POST'])
+def add_image():
+    """
+    This method used to add new image to the database
+    :return: result JSON
+    """
+    if request.method == 'POST':
+        try:
+            # Are the parameters valid?
+            if 'app-id' in request.args and 'image-path' in request.args:
+                app_id, image_path = get_add_image_params()
+                current_date = datetime.now()
+                # query and data preparation to check app_id is already in db or not
+                sql_query = "SELECT COUNT(app_id) FROM apps_case_study.images WHERE app_id=%s and image_path=%s;"
+                query_data: tuple = (app_id, image_path,)
+                # database crud operations
+                db_cursor, connection, query_data = fetch_data(sql_query, query_data)
+                close_connection(connection, db_cursor)
+                query_data: int = int(query_data[0][0])
+                # Is this given photo in database?
+                # The data is not in database
+                if query_data == 0:
+                    # query and data preparation
+                    sql_query:str = "INSERT INTO apps_case_study.images (app_id,image_path,created_date,updated_date) \
+                                VALUES(%s,%s,%s,%s);"
+                    current_date = datetime.now()
+                    query_data: tuple = (app_id, image_path, str(current_date.strftime('%Y-%m-%d %H:%M:%S')),
+                                         str(current_date.strftime('%Y-%m-%d %H:%M:%S')),)
+                    # database crud operations
+                    db_cursor, connection = create_record(sql_query, query_data)
+                    close_connection(connection, db_cursor)
+                    # prepare informative json reply
+                    informative_msg:dict={'message':"The image successfully added to database"}
+                    response = jsonify(informative_msg)
+                    # HTTP 201 -> Created
+                    response.status_code = 201
+
+                    return response
+
+                # The data is in database
+                else:
+                    # query and data preparation to update image path in database
+                    sql_query = "UPDATE apps_case_study.images SET image_path = %s, updated_date=%s  WHERE app_id = %s;"
+                    query_data: tuple = (image_path, str(current_date.strftime('%Y-%m-%d %H:%M:%S')), app_id)
+                    # database crud operations
+                    db_cursor, connection = create_record(sql_query, query_data)
+                    close_connection(connection, db_cursor)
+                    informative_msg: dict = {'message': "The image successfully updated to database"}
+                    response = jsonify(informative_msg)
+                    # HTTP 201 -> Created
+                    response.status_code = 201
+                    return response
+
+            # Parameters are invalid
+            else:
+                return 'Parameters are not valid'
+        except Exception as error:
+            return f'{error}'
 
 
 if __name__ == '__main__':
